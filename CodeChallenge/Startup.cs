0653b3763f9ed23c4c8cf53236fc9066c5f28c40
zeynepozturkmen <zeynepozturkmen62@gmail.com>
2023-12-23 20:37:46 +0300

    Add migrate command

--- CodeChallenge/Startup.cs
+++ CodeChallenge/Startup.cs
@@ -38,6 +38,12 @@ namespace CodeChallenge
                 optionsBuilder.MigrationsAssembly("CodeChallenge")), ServiceLifetime.Scoped
             );
 
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+
+            var dbContext = serviceProvider.GetRequiredService<CodeChallengeDbContext>();
+
+            dbContext.Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
